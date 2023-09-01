c22c494bac163bc006c8a192b9e4523822376c2e
jakegodsall <jake.edward.godsall@gmail.com>
2023-09-01 11:12:00 +0200

    Created Post entity

---
+++ src/main/java/com/jakegodsall/personalblog/entity/Post.java
@@ -0,0 +1,87 @@
+package com.jakegodsall.personalblog.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "Posts", uniqueConstraints = {@UniqueConstraint(columnNames = "title")})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    @Column(nullable = false)
+    private String content;
+    @Column(nullable = false)
+    private Date publishedDate;
+    @Column(nullable = false)
+    private Date lastEditedDate;
+
+    public Post() {}
+
+    public Post(long id,
+                String title,
+                String description,
+                String content,
+                Date publishedDate,
+                Date lastEditedDate) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.content = content;
+        this.publishedDate = publishedDate;
+        this.lastEditedDate = lastEditedDate;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public Date getLastEditedDate() {
+        return lastEditedDate;
+    }
+
+    public void setLastEditedDate(Date lastEditedDate) {
+        this.lastEditedDate = lastEditedDate;
+    }
+}
